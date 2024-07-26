e75375a9290b9c1c4c5ef9a235e9acb5d5f75c3b
Edison Tong <edisonjtong@gmail.com>
2024-07-25 19:22:45 -0600

    changes

--- Unit 3/objects/objects-exercise/script.js
+++ Unit 3/objects/objects-exercise/script.js
@@ -1,8 +1,8 @@
 // Objects Exercise here
 let person = {
-    firstName: "Lightning",
-    lastName: "McQueen",
-    hobby: "Tractor tipping",
+  firstName: "Lightning",
+  lastName: "McQueen",
+  hobby: "Tractor tipping",
 };
 
 console.log("Initial person:", person);
@@ -15,45 +15,43 @@ console.log("This persons previous job was:", person.previousJob);
 console.log("Final person:", person);
 
 let shapes = [
-    {
-        name: "chiliagon",
-        sides: 1000,
-    },
-    {
-        name: "Myriagon",
-        sides: 10000,
-    },
-    {
-        name: "Megagon",
-        sides: 1000000,
-    },
+  {
+    name: "chiliagon",
+    sides: 1000,
+  },
+  {
+    name: "Myriagon",
+    sides: 10000,
+  },
+  {
+    name: "Megagon",
+    sides: 1000000,
+  },
 ];
 
 let circle = {
-    name: "circle",
-    sides: 0,
+  name: "circle",
+  sides: 0,
 };
 shapes.push(circle);
 
 for (i = 0; i < shapes.length - 1; i++) {
-    console.log(`A ${shapes[i].name} has ${shapes[i].sides} sides`);
+  console.log(`A ${shapes[i].name} has ${shapes[i].sides} sides`);
 }
 console.log("Final shapes array:", shapes);
 
-let numSides = parseInt(
-    prompt("Please enter a number of sides you want to search for.")
-);
+let numSides = parseInt(prompt("Please enter a number of sides you want to search for."));
 let foundShape = null;
 
 for (let i = 0; i < shapes.length; i++) {
-    if (numSides === shapes[i].sides) {
-        foundShape = shapes[i];
-        break;
-    }
+  if (numSides === shapes[i].sides) {
+    foundShape = shapes[i];
+    break;
+  }
 }
 
 if (foundShape === null) {
-    console.log("No matching shape was found.");
+  console.log("No matching shape was found.");
 } else {
-    console.log(`We found a shape! It's the ${foundShape.name}`);
+  console.log(`We found a shape! It's the ${foundShape.name}`);
 }
