3f9486ebfc117e75d302cfebaabd748ef5570f2a
Edison Tong <edisonjtong@gmail.com>
2024-07-25 21:05:04 -0600

    completed workshop4

--- workshops/Workshop4/challenges/script.js
+++ workshops/Workshop4/challenges/script.js
@@ -1,48 +1,71 @@
 // Challenge 1: Given the below array of objects, complete the below tasks:
 
 let people = [
-    {
-        name: "Sandra",
-        hobby: "playing soccer",
-        phoneNumber: "123-456-7777",
-        shoeSize: 6,
-    },
-    {
-        name: "Bill",
-        hobby: "woodworking",
-        phoneNumber: "123-456-8888",
-    },
-    {
-        name: "Paul",
-        job: "marketing specialist",
-        phoneNumber: "123-456-9999",
-        shoeSize: 9,
-    },
-    {
-        name: "Ariel",
-        shoeSize: 5,
-        job: "barista",
-    },
-    {
-        name: "Ben",
-        hobby: "playing video games",
-        phoneNumber: "123-456-0000",
-    },
+  {
+    name: "Sandra",
+    hobby: "playing soccer",
+    phoneNumber: "123-456-7777",
+    shoeSize: 6,
+  },
+  {
+    name: "Bill",
+    hobby: "woodworking",
+    phoneNumber: "123-456-8888",
+  },
+  {
+    name: "Paul",
+    job: "marketing specialist",
+    phoneNumber: "123-456-9999",
+    shoeSize: 9,
+  },
+  {
+    name: "Ariel",
+    shoeSize: 5,
+    job: "barista",
+  },
+  {
+    name: "Ben",
+    hobby: "playing video games",
+    phoneNumber: "123-456-0000",
+  },
 ];
 
 // Challenge 1.1: Log a message to the console for each person who has a hobby
 // Example: Sandra likes playing soccer.
+for (let i = 0; i < people.length; i++) {
+  if (people[i].hobby) {
+    console.log(`${people[i].name} likes ${people[i].hobby}.`);
+  }
+}
 
 // Challenge 1.2: Log a message to the console for each person who has a shoeSize
 // Example: Sandra has a shoe size of 6.
 
+for (let i = 0; i < people.length; i++) {
+  if (people[i].shoeSize) {
+    console.log(`${people[i].name} has a shoe size of ${people[i].shoeSize}.`);
+  }
+}
+
 // Challenge 1.3: Log a message to the console for each person who has a phone number
 // Example: Sandra's phone number is 123-456-7777
+
+for (let i = 0; i < people.length; i++) {
+  if (people[i].phoneNumber) {
+    console.log(`${people[i].name}'s phone number is ${people[i].phoneNumber}.`);
+  }
+}
 
 // Challenge 2: Factorial
 // Declare a function called "factorial" that takes a number and returns its factorial.
 // The factorial of a number is calculated: 4! = 4 * 3 * 2 * 1 = 24
-function factorial(num) {}
+function factorial(num) {
+  let result = num;
+  for (let i = num - 1; i > 0; i--) {
+    result *= i;
+  }
+  return result;
+}
 
 // factorial Examples:
 console.log(factorial(1)); // 1
@@ -54,29 +77,37 @@ console.log(factorial(8)); // 40320
 // Practice: Farthest Driver
 // Declare a function farthestDriver which takes one input: an array of objects representing drivers
 // farthestDriver should return the name of the driver that's farthest from the user.
-function farthestDriver(drivers) {}
+function farthestDriver(drivers) {
+  let farthest = drivers[0];
+  for (let i = 1; i < drivers.length; i++) {
+    if (drivers[i].feetFromUser > farthest.feetFromUser) {
+      farthest = drivers[i];
+    }
+  }
+  return farthest.firstName;
+}
 
 let drivers = [
-    {
-        id: 101,
-        firstName: "Jenny",
-        feetFromUser: 5193,
-    },
-    {
-        id: 204,
-        firstName: "Nigel",
-        feetFromUser: 245,
-    },
-    {
-        id: 33,
-        firstName: "Andreas",
-        feetFromUser: 78,
-    },
-    {
-        id: 443,
-        firstName: "Boris",
-        feetFromUser: 8331,
-    },
+  {
+    id: 101,
+    firstName: "Jenny",
+    feetFromUser: 5193,
+  },
+  {
+    id: 204,
+    firstName: "Nigel",
+    feetFromUser: 245,
+  },
+  {
+    id: 33,
+    firstName: "Andreas",
+    feetFromUser: 78,
+  },
+  {
+    id: 443,
+    firstName: "Boris",
+    feetFromUser: 8331,
+  },
 ];
 
 console.log(farthestDriver(drivers)); // 443
@@ -85,7 +116,15 @@ console.log(farthestDriver(drivers)); // 443
 // Declare a function findKey which takes 2 inputs: an object and a data value.
 // The function finds and returns the key name in the object that corresponds to the
 // given data value. If the value is not found, the function returns null.
-function findKey(obj, value) {}
+function findKey(obj, value) {
+  let result = null;
+  for (let key in obj) {
+    if (obj[key] === value) {
+      result = key;
+    }
+  }
+  return result;
+}
 
 // findKey Examples:
 let myObject = { a: 1, b: 4, c: 3, d: 8, e: 13, f: 6 };
@@ -98,7 +137,15 @@ console.log(findKey(myObject, 13)); // e
 // and returns the number of letters in the string.
 // The number of letters does NOT include spaces.
 // Hint: Use the split() function: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
-function totalLetters(sentence) {}
+
+function totalLetters(sentence) {
+  let split = sentence.split(" ");
+  let total = 0;
+  for (let i = 0; i < split.length; i++) {
+    total += split[i].length;
+  }
+  return total;
+}
 
 // totalLetters Examples:
 let sentence1 = "This is an example of a sentence";
--- workshops/Workshop4/demo/script.js
+++ workshops/Workshop4/demo/script.js
@@ -5,11 +5,11 @@ function nearestStore(stores) {}
 
 // Examples:
 let stores = [
-    { id: 112, name: "Store A", metersFromUser: 243 },
-    { id: 267, name: "Store B", metersFromUser: 59 },
-    { id: 362, name: "Store C", metersFromUser: 342 },
-    { id: 335, name: "Store D", metersFromUser: 8943 },
-    { id: 321, name: "Store E", metersFromUser: 200 },
+  { id: 112, name: "Store A", metersFromUser: 243 },
+  { id: 267, name: "Store B", metersFromUser: 59 },
+  { id: 362, name: "Store C", metersFromUser: 342 },
+  { id: 335, name: "Store D", metersFromUser: 8943 },
+  { id: 321, name: "Store E", metersFromUser: 200 },
 ];
 
 console.log(nearestStore(stores)); // Output: 267
@@ -18,12 +18,16 @@ console.log(nearestStore(stores)); // Output: 267
 // Write a function logAll that takes an object an logs all of the object's properties
 // and values to the console
 // Example: name: Alice
-function logAll(obj) {}
+function logAll(obj) {
+  for (let key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+  }
+}
 
 let person = {
-    name: "Alice",
-    age: 30,
-    occupation: "Engineer",
+  name: "Alice",
+  age: 30,
+  occupation: "Engineer",
 };
 logAll(person);
 // Output:
@@ -32,9 +36,9 @@ logAll(person);
 // occupation: Engineer
 
 let car = {
-    make: "Toyota",
-    model: "Camry",
-    year: 2020,
+  make: "Toyota",
+  model: "Camry",
+  year: 2020,
 };
 logAll(car);
 // Output:
@@ -43,9 +47,9 @@ logAll(car);
 // year: 2020
 
 let book = {
-    title: "1984",
-    author: "George Orwell",
-    pages: 328,
+  title: "1984",
+  author: "George Orwell",
+  pages: 328,
 };
 logAll(book);
 // Output:
