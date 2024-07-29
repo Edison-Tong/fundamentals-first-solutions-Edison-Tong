6e1d2e1864700bdd53528df80ed565d9b16de449
Edison Tong <edisonjtong@gmail.com>
2024-07-29 17:31:21 -0600

    completed highe order programming exercise

--- Unit 4/higher-order-programming/higher-order-programming-exercise/script.js
+++ Unit 4/higher-order-programming/higher-order-programming-exercise/script.js
@@ -3,31 +3,58 @@
 // Calculator Exercise:
 // Pre-defined Functions
 let addition = function (num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 };
 
 let subtraction = function (num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 };
 
 let multiplication = function (num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 };
 
 let division = function (num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
 };
 
 // Define the calculator function here:
 
+function calculator(num1, num2, operation) {
+  console.log(operation(num1, num2));
+}
+calculator(3, 5, multiplication);
+calculator(7, 5, addition);
+calculator(35, 8, subtraction);
+calculator(681, 3, division);
+
 // Poor man's forEach Exercise
+
+function forEach(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    console.log(func(array[i]));
+  }
+}
+
+let yellIt = function (string) {
+  return string.toUpperCase();
+};
+let words = ["what", "am", "I", "doing"];
+
+forEach(words, yellIt);
 
 // Print Each Song Title Exercise
 // Example song objects here
 let exampleSongs = [
-    { title: "Intro", artist: "The xx", minutes: 127 },
-    { title: "Free Fallin'", artist: "Tom Petty", minutes: 256 },
-    { title: "What You Know", artist: "Two Door Cinema Club", minutes: 191 },
-    { title: "Closer", artist: "The Chainsmokers", minutes: 244 },
-    { title: "Gooey", artist: "Glass Animals", minutes: 289 },
+  { title: "Intro", artist: "The xx", minutes: 127 },
+  { title: "Free Fallin'", artist: "Tom Petty", minutes: 256 },
+  { title: "What You Know", artist: "Two Door Cinema Club", minutes: 191 },
+  { title: "Closer", artist: "The Chainsmokers", minutes: 244 },
+  { title: "Gooey", artist: "Glass Animals", minutes: 289 },
 ];
+
+let printSongTitle = function (song) {
+  console.log(song.title);
+};
+
+exampleSongs.forEach(printSongTitle);
