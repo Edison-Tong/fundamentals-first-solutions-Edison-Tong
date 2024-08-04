496b1252b636bd7c04f63bc359337e0e613fa473
Edison Tong <edisonjtong@gmail.com>
2024-08-03 18:50:34 -0600

    completed Dynamic content assignment

--- Unit 6/dynamic-content/dynamic-content-exercise/script.js
+++ Unit 6/dynamic-content/dynamic-content-exercise/script.js
@@ -1,1 +1,15 @@
 // Dynamic Content Exercise here
+let header = $("h1").text("Shopping list");
+
+let listItems = $("li").attr("class", "shopping-list-item");
+
+let button = $("#log-me-button");
+button.css("background-color", "lightgreen");
+
+function afterClicked(event) {
+  let favFood = $("#favorite-food-input").val();
+
+  console.log(`The users favorite food is ${favFood}`);
+}
+
+button.on("click", afterClicked);
