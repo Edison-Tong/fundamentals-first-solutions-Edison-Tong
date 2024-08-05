83c58777a2b34900c6105a7a9a324ba531e885e2
Edison Tong <edisonjtong@gmail.com>
2024-08-05 15:02:22 -0600

    completed JSON APIs exercise

--- Unit 6/json-apis/json-apis-exercise/script.js
+++ Unit 6/json-apis/json-apis-exercise/script.js
@@ -1,1 +1,12 @@
 // JSON APIs Exercise here
+$.getJSON("https://jsonplaceholder.typicode.com/users/6", function (response) {
+  console.log(response);
+});
+
+function getUser(userID) {
+  $.getJSON(`https://jsonplaceholder.typicode.com/users/${userID}`, function (response) {
+    console.log(response);
+  });
+}
+
+getUser(3);
